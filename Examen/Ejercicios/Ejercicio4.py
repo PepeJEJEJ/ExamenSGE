33e86ea77cd93c71176d019e04ee7597c77212ee
Pepe <jah0005@alu.medac.es>
2026-02-06 11:15:44 +0100

    Create Ejercicio4.py

---
+++ Examen/Ejercicios/Ejercicio4.py
@@ -0,0 +1,17 @@
+class ElProducto:
+    def __init__(self, nombre, precio, stock):
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+Producto1 = ElProducto ("RAM", 29124982410914289421809089421094812, 10)
+Producto2 = ElProducto ("Teclado", 33, 1)
+
+def mostrar_datos(self Producto1):
+    print("Nombre: {self.nombre} Precio: {self.precio} Stock{self.stock}")
+def venderUds(self Producto2):
+    cesta = int(input("Cuanto quieres: "))
+    if cesta >self stock:
+        print("INSUFICIENTE STOCK")
+    elif cesta < stock:
+        stock-cesta
+        print(stock)
